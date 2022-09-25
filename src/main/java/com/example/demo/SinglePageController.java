9d3a651238953fa823b15485f0b8cb66d3980373
Take327 <gatewayseed2@gmail.com>
2022-09-25 12:53:29 +0900

    react-router追加

--- src/main/java/com/example/demo/SinglePageController.java
+++ src/main/java/com/example/demo/SinglePageController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SinglePageController {
+	//すべてのPathのGetリクエストでSPAを返却する
 	@GetMapping("/**/{path:[^.]*}")
     public String any() {
         return "forward:/index.html";
